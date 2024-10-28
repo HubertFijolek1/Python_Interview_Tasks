5526339dcd936afd5ffe67c4a4ce91cd0f0e0d05
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-10-28 08:21:06 +0100

    Add functions to remove punctuation from a sentence
    Introduced three functions: `remove_punctuation`, `remove_punctuation2`, and `remove_punctuation3` to handle punctuation removal using different approaches. These functions utilize the `string` module, manual character checking, and regular expressions, respectively.

---
+++ junior/core_python_tasks/task19.py
@@ -0,0 +1,17 @@
+### 9. **Function that takes a sentence and removes all punctuation**
+
+import string
+def remove_punctuation(s):
+    return s.translate(str.maketrans('', '', string.punctuation))
+
+def remove_punctuation2(s):
+    punctuation = '''!()-[]{};:'"\\,<>./?@#$%^&*_~'''
+    result = ""
+    for char in s:
+        if char not in punctuation:
+            result += char
+    return result
+
+import re
+def remove_punctuation3(s):
+        return re.sub(r'[^\\w\\s]', '', s)
