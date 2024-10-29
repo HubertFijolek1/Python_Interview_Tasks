88247bbd5424b73a65b5707a896fd052e3d03cc4
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-10-29 10:02:49 +0100

    Add bubble sort implementation
    Introduced two functions: `bubble_sort` for iterative sorting and `bubble_sort_recursive` for recursive sorting. These functions sort an array in ascending order using the bubble sort algorithm.

---
+++ junior/data_structures_and_algorithms/task3.py
@@ -0,0 +1,20 @@
+### 3. **Implement bubble sort**
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def bubble_sort_recursive(arr, n=None):
+    if n is None:
+        n = len(arr)
+    if n == 1:
+        return arr
+    for i in range(n - 1):
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    return bubble_sort_recursive(arr, n - 1)
+
