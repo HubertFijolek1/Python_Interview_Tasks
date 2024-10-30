a45b3539e9ce582dd80ee412c9d0ac9d6fb6d7ef
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-10-30 17:06:41 +0100

    Remove unnecessary comments for clarity
    Deleted redundant comments indicating solution numbers in task1_largest_number.py. This improves code readability and focuses on the implementation details directly.

--- junior/core_python_tasks/task1_largest_number.py
+++ junior/core_python_tasks/task1_largest_number.py
@@ -2,7 +2,6 @@
 def largest_number(lst):
     return max(lst)
 
-### Solution 2
 def largest_number2(lst):
     largest = lst[0]
     for num in lst:
@@ -10,7 +9,6 @@ def largest_number2(lst):
             largest = num
     return largest
 
-###Solution 3
 from functools import reduce
 
 def largest_number3(lst):
