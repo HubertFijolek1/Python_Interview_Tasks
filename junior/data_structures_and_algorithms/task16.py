1443e492b8019b3be4281cd44a9f249aba79d54b
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-03 09:57:48 +0100

    Add matrix rotation functions
    Introduce three distinct functions for rotating a matrix by 90 degrees: `rotate_matrix` for returning a new rotated matrix, `rotate_matrix_inplace` for in-place rotation, and `rotate_matrix_transpose_reverse` for rotating using transpose and reverse method. These functions provide multiple approaches for handling 2D list rotations efficiently.

---
+++ junior/data_structures_and_algorithms/task16.py
@@ -0,0 +1,21 @@
+### 6. **Function that rotates a matrix (2D list) by 90 degrees**
+
+def rotate_matrix(matrix):
+    return [list(reversed(col)) for col in zip(*matrix)]
+
+def rotate_matrix_inplace(matrix):
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = temp
+    return matrix
+
+def rotate_matrix_transpose_reverse(matrix):
+    matrix[:] = [list(row) for row in zip(*matrix)]
+    for row in matrix:
+        row.reverse()
+    return matrix
