940cb99bd08c4539754a678337cd329c3e96488b
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-03 10:00:04 +0100

    Add function to merge dictionaries with combined values
    Implemented two methods for merging dictionaries by summing the values of matching keys. The first method uses defaultdict and a loop, while the second employs dictionary comprehension for a more concise approach.

---
+++ junior/data_structures_and_algorithms/task18.py
@@ -0,0 +1,14 @@
+### 8. **Function that merges two dictionaries, combining values for matching keys**
+
+from collections import defaultdict
+def merge_dicts(dict1, dict2):
+    result = defaultdict(int)
+    for d in (dict1, dict2):
+        for key, value in d.items():
+            result[key] += value
+    return dict(result)
+
+def merge_dicts2(dict1, dict2):
+    return {key: dict1.get(key, 0) + dict2.get(key, 0) for key in set(dict1) | set(dict2)}
+
+
