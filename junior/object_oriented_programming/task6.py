1528c27464d261fb9a5c4f42b1104c20c9c59195
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-10 15:09:14 +0100

    # Class hierarchy for Shape, Square, and Triangle with area() method - Using `super()` for shared attributes:

--- junior/object_oriented_programming/task6.py
+++ junior/object_oriented_programming/task6.py
@@ -22,3 +22,33 @@ class Triangle(Shape):
 
     def area(self):
         return 0.5 * self.base * self.height
+
+
+# Solution 2 (Using `super()` for shared attributes):
+
+class Shape2:
+    def __init__(self, name):
+        self.name = name
+
+    def area(self):
+        pass
+
+
+class Square2(Shape2):
+    def __init__(self, side):
+        super().__init__("Square")
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+
+class Triangle2(Shape2):
+    def __init__(self, base, height):
+        super().__init__("Triangle")
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return 0.5 * self.base * self.height
+
