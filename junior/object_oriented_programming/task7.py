16cede5a63090cd0195b629a33d4b51400eed297
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-10 15:12:41 +0100

    Adding Class ShoppingCart for adding, removing, viewing items, and calculating the total price - Using a dictionary with quantitie

--- junior/object_oriented_programming/task7.py
+++ junior/object_oriented_programming/task7.py
@@ -18,3 +18,27 @@ class ShoppingCart:
 
     def total_price(self):
         return sum(item['price'] for item in self.items)
+
+
+#Solution 2(Using a dictionary with quantities) :
+
+class ShoppingCart2:
+    def __init__(self):
+        self.items = {}
+
+    def add_item(self, item, price, quantity=1):
+        if item in self.items:
+            self.items[item]['quantity'] += quantity
+        else:
+            self.items[item] = {'price': price, 'quantity': quantity}
+
+    def remove_item(self, item):
+        if item in self.items:
+            del self.items[item]
+
+    def view_cart(self):
+        return self.items
+
+    def total_price(self):
+        return sum(item['price'] * item['quantity'] for item in self.items.values())
+
