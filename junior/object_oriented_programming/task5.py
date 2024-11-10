ab30983219063df4c4ffaed32a8487af908d02fa
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-10 15:03:03 +0100

    Adding Class implementing a simple stack with push and pop operations - Using a linked list for stack

--- junior/object_oriented_programming/task5.py
+++ junior/object_oriented_programming/task5.py
@@ -11,3 +11,26 @@ class Stack:
 
     def pop(self):
         return self.stack.pop() if self.stack else None
+
+# Solution 2 (Using a linked list for stack):
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack2:
+    def __init__(self):
+        self.head = None
+
+    def push(self, item):
+        new_node = Node(item)
+        new_node.next = self.head
+        self.head = new_node
+
+    def pop(self):
+        if self.head is None:
+            return None
+        data = self.head.data
+        self.head = self.head.next
+        return data
