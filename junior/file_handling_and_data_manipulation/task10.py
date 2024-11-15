df05de95b35f7e84d40a1e16ee619529711820be
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-15 12:20:22 +0100

    10. Function to concatenate multiple text files into a single file - Solution 2 (Using fileinput)

--- junior/file_handling_and_data_manipulation/task10.py
+++ junior/file_handling_and_data_manipulation/task10.py
@@ -7,3 +7,13 @@ def concatenate_files(output_file, *input_files):
         for fname in input_files:
             with open(fname, 'r') as infile:
                 outfile.write(infile.read())
+
+# Solution 2 (Using fileinput):
+
+import fileinput
+
+def concatenate_files2(output_file, *input_files):
+    with open(output_file, 'w') as outfile:
+        for line in fileinput.input(files=input_files):
+            outfile.write(line)
+
