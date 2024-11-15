ccc23af640c03535ccf0128f703fb5e40000b6d5
HubertFijolek1 <hubertfijolek1@gmail.com>
2024-11-15 12:02:28 +0100

    9. Function to find and replace a word in a text file - Solution 2 (Using line-by-line modification)

--- junior/file_handling_and_data_manipulation/task9.py
+++ junior/file_handling_and_data_manipulation/task9.py
@@ -8,3 +8,12 @@ def find_and_replace(filename, old_word, new_word):
     content = content.replace(old_word, new_word)
     with open(filename, 'w') as file:
         file.write(content)
+
+# Solution 2 (Using line-by-line modification):
+
+def find_and_replace2(filename, old_word, new_word):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line.replace(old_word, new_word))
